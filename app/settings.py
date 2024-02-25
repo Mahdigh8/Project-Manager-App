5fd2591a838ee00039c5b474f9d97925aeeb9fad
mahdigh8 <mahdighazaei8@gmail.com>
2024-02-25 19:59:55 +0330

    add: auth_token and app

--- app/settings.py
+++ app/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    ## Apps
-    "core",
     ## Third-party Packages
     "rest_framework",
+    "rest_framework.authtoken",
+    ## Apps
+    "core",
+    "user",
 ]
 
 MIDDLEWARE = [
