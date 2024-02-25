f0dae10c66f3dd681ad680339a3ecae7724830aa
mahdigh8 <mahdighazaei8@gmail.com>
2024-02-25 15:55:54 +0330

    add: CustomUser model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    ## put username in required_fields to ask for it when creating superuser in command line.
+    REQUIRED_FIELDS = ["username"]
