7c9aa664af8361b5e5f3bfe7a8ed639cdc5256b0
mahdigh8 <mahdighazaei8@gmail.com>
2024-02-28 17:13:46 +0330

    Add: Team and TeamMember models to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Team, TeamMember
 
 # Register your models here.
 
 admin.site.register(CustomUser)
+admin.site.register(Team)
+admin.site.register(TeamMember)
