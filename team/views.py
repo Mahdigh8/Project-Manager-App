f2b2fd19b94fa4eaa8ade6d2504acf37c8b86afe
mahdigh8 <mahdighazaei8@gmail.com>
2024-02-29 22:46:57 +0330

    Add: viewset and router for Team model

--- team/views.py
+++ team/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from core.models import Team
+from . import serializers
+from .permissions import IsAllowedToEdit, IsAllowedToDelete
 
-# Create your views here.
+
+class TeamViewSet(ModelViewSet):
+    serializer_class = serializers.TeamSerializer
+    queryset = Team.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAllowedToEdit, IsAllowedToDelete]
+
+    def get_queryset(self):
+        return Team.objects.filter(member__user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return serializers.TeamListSerializer
+        return self.serializer_class
