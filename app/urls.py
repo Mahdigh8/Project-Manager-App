2156e6a927d8dd4ea410edfc11982cf2b93509c0
mahdigh8 <mahdighazaei8@gmail.com>
2024-02-29 22:48:03 +0330

    Fix: urls for team app

--- app/urls.py
+++ app/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-    path("team/", include("team.urls")),
+    path("api/team/", include("team.urls")),
 ]
