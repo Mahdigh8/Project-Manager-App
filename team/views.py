8eadb5bbbb13f71f2199a0df2d8bec059c12e7b7
mahdigh8 <mahdighazaei8@gmail.com>
2024-03-01 16:35:32 +0330

    Add: action method for listing and adding members to team

--- team/views.py
+++ team/views.py
@@ -1,9 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from core.models import Team
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from core.models import Team, TeamMember
 from . import serializers
-from .permissions import IsAllowedToEdit, IsAllowedToDelete
+from .permissions import IsAllowedToEdit, IsAllowedToDelete, IsAllowedToEditMembers
 
 
 class TeamViewSet(ModelViewSet):
@@ -19,3 +22,37 @@ class TeamViewSet(ModelViewSet):
         if self.action == "list":
             return serializers.TeamListSerializer
         return self.serializer_class
+
+    @action(
+        detail=True,
+        methods=["post", "get"],
+        serializer_class=serializers.TeamMemberSerializer,
+    )
+    def members(self, request, pk=None):
+        """Action for crud of team members"""
+        team = self.get_object()
+        if request.method == "GET":
+            team_members = TeamMember.objects.filter(team=team)
+            serializer = serializers.TeamMemberSerializer(team_members, many=True)
+            return Response(serializer.data)
+
+        elif request.method == "POST":
+            context = {"request": self.request}
+            serializer = serializers.TeamMemberSerializer(
+                data=request.data, context=context, many=True
+            )
+            if serializer.is_valid():
+                serializer.save(team=team)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == "members":
+            permission_classes = [IsAuthenticated, IsAllowedToEditMembers]
+        else:
+            permission_classes = self.permission_classes
+        return [permission() for permission in permission_classes]
