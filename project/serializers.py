f0489d3e6422dd87c9470f6be1cf01689b963fc1
mahdigh8 <mahdighazaei8@gmail.com>
2024-03-03 16:45:18 +0330

    Add: team_id field to project serializer

--- project/serializers.py
+++ project/serializers.py
@@ -15,10 +15,11 @@ class ProjectSerializer(serializers.ModelSerializer):
     team = serializers.HyperlinkedRelatedField(
         view_name="team:team-detail", read_only=True
     )
+    team_id = serializers.IntegerField(required=False, write_only=True)
 
     class Meta:
         model = Project
-        fields = ["id", "name", "description", "team", "deadline"]
+        fields = ["id", "name", "team_id", "description", "team", "deadline"]
         extra_kwargs = {"description": {"required": False}}
 
     def create(self, validated_data):
