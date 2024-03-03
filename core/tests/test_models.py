49e38b8379e3ccd7061482185fcd5a7ad9aa0f7e
mahdigh8 <mahdighazaei8@gmail.com>
2024-03-03 13:33:03 +0330

    Remove: created_by field in project tests

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -70,26 +70,17 @@ class TestProjectModel(TestCase):
             "name": "Test team",
             "public_edit": "ADMIN",
         }
-        user_payload = {
-            "email": "test@example.com",
-            "username": "testuser1",
-            "password": "Testpass123",
-        }
         self.team = models.Team.objects.create(**team_payload)
-        user = get_user_model().objects.create_user(**user_payload)
-        self.member = models.TeamMember.objects.create(team=self.team, user=user)
 
     def test_create_project(self):
         """Test creating a project object"""
         payload = {
             "name": "Test project",
             "team": self.team,
-            "created_by": self.member,
             "deadline": time.strftime("%Y-%m-%d %H:%M%z"),
         }
         project = models.Project.objects.create(**payload)
         self.assertEqual(project.name, payload["name"])
         self.assertEqual(project.description, "")
         self.assertEqual(project.team, payload["team"])
-        self.assertEqual(project.created_by, payload["created_by"])
         self.assertEqual(project.deadline, payload["deadline"])
