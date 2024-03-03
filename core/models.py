5d3f2aeb5aa7ee80469bbaf087780d78fa3fa584
mahdigh8 <mahdighazaei8@gmail.com>
2024-03-03 12:10:05 +0330

    Add: Project model and migration file

--- core/models.py
+++ core/models.py
@@ -21,7 +21,7 @@ class Team(models.Model):
     description = models.TextField(blank=True)
     ## Who can edit the team name and description
     public_edit = models.CharField(
-        max_length=10, choices=EDIT_PERMISSION_CHOICES, default="ALL"
+        max_length=5, choices=EDIT_PERMISSION_CHOICES, default="ALL"
     )
     ## Who can invite members to the team and delete the team
     privacy_edit = models.CharField(
@@ -42,3 +42,17 @@ class TeamMember(models.Model):
 
     def __str__(self):
         return f"{self.user.username} -> {self.team.name}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    ## project should not be deleted when the creator leave the team.
+    ## that's why models.SET_NULL is used
+    created_by = models.ForeignKey(TeamMember, null=True, on_delete=models.SET_NULL)
+    deadline = models.DateTimeField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}-{self.team.name}"
