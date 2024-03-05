de2b10c53947f3d5e907865912428a432afc4395
mahdigh8 <mahdighazaei8@gmail.com>
2024-03-05 16:51:10 +0330

    Add: task serializers

--- project/serializers.py
+++ project/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from core.models import Project, Team
+from core.models import Project, Team, Task
+from .custom_serializer_fields import TaskDetailHyperlink
 
 
 class ProjectListSerializer(serializers.Serializer):
@@ -35,3 +36,34 @@ class ProjectSerializer(serializers.ModelSerializer):
             team = Team.objects.get(pk=team_id)
             validated_data["team"] = team
         return super().update(instance, validated_data)
+
+
+class TaskListSerializer(serializers.ModelSerializer):
+    """Serializer for listing task objects"""
+
+    assigned_to = serializers.CharField(source="assigned_to.user.username")
+    url = TaskDetailHyperlink(view_name="project:task-detail")
+
+    class Meta:
+        model = Task
+        fields = ["id", "title", "status", "assigned_to", "due_date", "url"]
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    """Serializer for listing task objects"""
+
+    assigned_to = serializers.CharField(source="assigned_to.user.username")
+    created_by = serializers.CharField(source="created_by.user.username")
+
+    class Meta:
+        model = Task
+        fields = [
+            "id",
+            "title",
+            "status",
+            "assigned_to",
+            "created_by",
+            "created_at",
+            "description",
+            "due_date",
+        ]
