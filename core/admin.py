819f5a81a08a62cc5ab307c035c0de42070870a8
mahdigh8 <mahdighazaei8@gmail.com>
2024-03-05 16:48:39 +0330

    Add: project, task and comment models to admin page

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import CustomUser, Team, TeamMember
+from .models import CustomUser, Team, TeamMember, Project, Task, Comment
 
 # Register your models here.
 
 admin.site.register(CustomUser)
 admin.site.register(Team)
 admin.site.register(TeamMember)
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Comment)
